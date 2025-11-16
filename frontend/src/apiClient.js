0319856dbe661a4f850227985136975b751d7520
Developer-1 <suyogkumawat1999@gmail.com>
2025-11-17 01:38:33 +0530

    added

--- frontend/src/apiClient.js
+++ frontend/src/apiClient.js
@@ -13,18 +13,3 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: window.APP_API_URL,
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-export default api;
